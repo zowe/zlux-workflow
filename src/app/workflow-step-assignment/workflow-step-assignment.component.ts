297f0b7dbcc8a6d1d77a6dd4dace5a1eb8cf7ad9
Leonty Chudinov <lchudinov@rocketsoftware.com>
2018-09-12 13:48:21 +0500

    Implement assignment view
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- src/app/workflow-step-assignment/workflow-step-assignment.component.ts
+++ src/app/workflow-step-assignment/workflow-step-assignment.component.ts
@@ -9,11 +9,14 @@
 */
 
 import {
-   Component,
-   Input,
-   OnInit
+  Component,
+  Input,
+  OnInit
 } from '@angular/core';
+import { logger } from '../shared/logger';
+import { LoggerService } from '../shared/logger-service';
 import { WorkflowStep } from '../shared/workflow-step';
+import { ZosmfWorkflowService } from './../shared/zosmf-workflow-service';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -23,12 +26,41 @@ import { WorkflowStep } from '../shared/workflow-step';
 })
 export class WorkflowStepAssignmentComponent implements OnInit {
   @Input() step: WorkflowStep;
-  constructor() { }
+  userid: string;
+  constructor(
+    private zosmfWorkflowService: ZosmfWorkflowService,
+    private loggerService: LoggerService
+  ) { }
 
   ngOnInit() {
   }
 
-}/*
+  get assignees(): string[] {
+    if (this.step.assignees) {
+      return this.step.assignees.toLowerCase().split(', ');
+    }
+    return [];
+  }
+
+  addAssignee(userid: string): void {
+    this.zosmfWorkflowService.assignStepToUser(this.step, userid)
+      .finally(() => this.userid = '')
+      .subscribe(
+        () => logger.info(`step ${this.step.name} assigned to ${userid}`),
+        (err) => this.loggerService.zosmfError(err)
+      );
+  }
+
+  removeAssignee(userid: string): void {
+    this.zosmfWorkflowService.removeUserFromStepAssignees(this.step, userid)
+      .subscribe(
+        () => logger.info(`${userid} has been removed from assignees list of ${this.step.name}`),
+        (err) => this.loggerService.zosmfError(err)
+      );
+  }
+
+}
+/*
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
