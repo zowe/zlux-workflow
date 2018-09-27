114101da8b6e93c736793b5721d76bc7f7e7912f
Leonty Chudinov <34669449+lchudinov@users.noreply.github.com>
2018-09-27 14:15:36 +0500

    Add missing copyright statements
    Signed-off-by: Leonty Chudinov <34669449+lchudinov@users.noreply.github.com>

--- src/app/workflow-step-container/workflow-step-container.component.ts
+++ src/app/workflow-step-container/workflow-step-container.component.ts
@@ -1,3 +1,13 @@
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
+
 import {
   AfterContentInit,
   AfterViewInit,
@@ -97,3 +107,13 @@ export class WorkflowStepContainerComponent
     this.selectedView = view;
   }
 }
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
