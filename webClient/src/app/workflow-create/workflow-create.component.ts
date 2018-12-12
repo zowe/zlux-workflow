290de3c9b3706ece24729ef1b9c40bcb1eba8f54
Leonty Chudinov <34669449+lchudinov@users.noreply.github.com>
2018-12-12 11:28:48 +0500

    Improve error handling in Workflow creation dialog
    * don't hide the dialog if an error occurred
    * issue a message when a new workflow has been created successfully
    
    Signed-off-by: Leonty Chudinov <34669449+lchudinov@users.noreply.github.com>

--- webClient/src/app/workflow-create/workflow-create.component.ts
+++ webClient/src/app/workflow-create/workflow-create.component.ts
@@ -3,9 +3,9 @@
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
@@ -113,11 +113,11 @@ export class WorkflowCreateComponent implements OnInit {
       .subscribe(
       data => {
         this.initWorkflowList.emit();
+        this.loggerService.info(`Workflow "${actionData.workflowName}" has been created`);
         this.viewCreateWorkflow.emit(false);
       },
       error => {
         this.loggerService.zosmfError(error);
-        this.viewCreateWorkflow.emit(false);
       });
   }
 
@@ -139,8 +139,8 @@ export class WorkflowCreateComponent implements OnInit {
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
